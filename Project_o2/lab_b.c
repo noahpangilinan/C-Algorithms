124f5ab5722e7471646ba7a9551ca182264f70e4
Noah Pangilinan <noahpangilinan1@gmail.com>
2025-01-17 19:42:03 -0500

    Lab2

---
+++ Project_o2/lab_b.c
@@ -0,0 +1,102 @@
+/****************************************************************************
+Lab 2 student file
+ 10/05/2019 R. Repka    Initial release
+****************************************************************************/
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+/*---------------------------------------------------------------------------
+  Student data structure
+---------------------------------------------------------------------------*/
+struct bandMember {
+    char f_name[256];
+    char l_name[256];
+    int  year;
+};
+
+union anumber1 { float floating_point; int integer; };
+struct anumber2 {float floating_point; int integer;};
+
+
+int main(int argc, char *argv[]) {
+    /*------------------------------------------------------------------------
+     *Struct vs Union
+    ------------------------------------------------------------------------*/
+    union anumber1 aNumber1;
+    aNumber1.floating_point = 6.0;
+    aNumber1.integer = 5;
+    int size = sizeof(aNumber1);
+    printf("UNION \nFloat: %f, Int: %i, Size: %i bytes", aNumber1.floating_point, aNumber1.integer, size);
+
+    struct anumber2 aNumber2;
+    aNumber2.floating_point = 6.0;
+    aNumber2.integer = 5;
+    size = sizeof(aNumber2);
+    printf("\n\nSTRUCT \nFloat: %f, Int: %i, Size: %i bytes\n", aNumber2.floating_point, aNumber2.integer, size);
+    /*---------------------------------------------------------------------------
+      Beatles for loop
+    ---------------------------------------------------------------------------*/
+    typedef struct bandMember band;
+    band beatles[] = {
+        {"John", "Lennon", 1940},
+        {"Paul","McCartney", 1942},
+        {"George", "Harrison", 1943},
+        {"Ringo", "Starr", 1940}
+    };
+
+    const int length = sizeof(beatles) / sizeof(beatles[0]);
+    printf("\nBEATLES FOR LOOP:");
+    for (int i = 0; i < length; i++) {
+        char firstname [sizeof(beatles[i].f_name)];
+        char lastname [sizeof(beatles[i].f_name)];
+        strncpy(firstname, beatles[i].f_name, sizeof firstname - 1);
+        strncpy(lastname, beatles[i].l_name, sizeof lastname - 1);
+
+        printf("\nBeatle %s %s born %i", firstname, lastname, beatles[i].year);
+    }
+        /*---------------------------------------------------------------------------
+          Beatles run on string for loop
+        ---------------------------------------------------------------------------*/
+    printf("\n\nBEATLES RUN ON STRING:");
+    char beatles_firstnames [256] = "";
+    unsigned long dest = 256-1;
+    for (int i = 0; i < length; i++) {
+        unsigned long firstname_size = strlen(beatles[i].f_name);
+
+        strncat(beatles_firstnames, beatles[i].f_name, dest);
+        dest -= firstname_size;
+    }
+    printf("\n%s", beatles_firstnames);
+    /*---------------------------------------------------------------------------
+        Find "Ringo" index
+      ---------------------------------------------------------------------------*/
+    printf("\n\nRINGO INDEX:");
+    char ringo[sizeof("Ringo")-1] = "Ringo";
+    for (int i = 0; i < length; i++) {
+        if (strncmp(ringo, beatles[i].f_name, sizeof ringo - 1) == 0) {
+            printf("\nThe Ringo index is %i", i);
+        }
+
+    }
+    /*---------------------------------------------------------------------------
+        Count to 255 without char or int and without checking for 255 specifically
+      ---------------------------------------------------------------------------*/
+    printf("\n\nCOUNTING TO 255:\n");
+    uint8_t counter = 1;
+    while (counter != 0 ) {
+        printf("%i ", counter);
+        counter++;
+    }
+    /*---------------------------------------------------------------------------
+        Code guaranteed to create a 64 bit long integer on ANY machine and prove the
+        value is 8 bytes long.
+      ---------------------------------------------------------------------------*/
+    int64_t sixtyfourbitint;
+    printf("\n\nSIXTY FOUR BIT INT:");
+    unsigned long sizeofsixtyfourbitint = sizeof(sixtyfourbitint);
+    printf("\n64 bit long integer size in bytes (using sizeof()): %lu" , sizeofsixtyfourbitint);
+
+
+}
+
---
+++ Project_o2/lab_c.c
@@ -0,0 +1,60 @@
+/****************************************************************************
+Lab 2 student file
+
+ To enable gdb logging
+   gdb <binary>
+   set logging file <file>
+   set logging overwrite on
+   set logging on
+
+         gcc -std=c99  -g -O0 lab_d.c -o lab_d
+
+ 10/05/2019 R. Repka    Initial release
+****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+    char *text_p;        // do not change
+
+    /*------------------------------------------------------------------------
+      String with Malloc
+    ------------------------------------------------------------------------*/
+    // char is 1 byte, so sizeof(char) * 12 gives 12 bytes
+    text_p = (char *)malloc(12);
+    if (text_p == NULL) {
+        perror("ERROR: malloc failed");
+        return 1;  // Exit the program if memory allocation fails
+    }
+
+        strncpy(text_p, "CMPE380 lab malloc try 1.", sizeof(text_p)-1);
+
+    printf("'%s'", text_p);
+    /*------------------------------------------------------------------------
+          String with realloc
+     ------------------------------------------------------------------------*/
+    text_p = (char *)realloc(text_p, 20);
+    if (text_p == NULL) {
+        perror("ERROR: malloc failed");
+        return -1;  // Exit the program if memory allocation fails
+    }
+
+    strncat(text_p, "--Realloc", 20 - strlen(text_p));
+    printf("\n");
+    printf("'%s'", text_p);
+
+    free(text_p);
+    /*------------------------------------------------------------------------
+              String to int/float conversion
+    ------------------------------------------------------------------------*/
+    char numstr[11] = " 0005.6000 ";
+    int integer = atoi(numstr);
+    float floatingpoint = atof(numstr);
+
+    printf("\n\n' 0005.6000 ' to integer: %i\n", integer);
+    printf("' 0005.6000 ' to integer: %f\n", floatingpoint);
+
+    return 0;
+}
